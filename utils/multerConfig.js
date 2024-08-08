9cd63db957833255e327f6f5c326fe05e81d50aa
Akshay sharma <akshaysharma5432@gmail.com>
2024-08-07 23:53:34 -0400

    images issue fix log--1

--- utils/multerConfig.js
+++ utils/multerConfig.js
@@ -3,13 +3,16 @@ const path = require("path");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, "..", "public", "images"));
+    const destPath = path.join(__dirname, "..", "public", "images");
+    console.log("Saving file to:", destPath);
+    cb(null, destPath);
   },
   filename: (req, file, cb) => {
-    cb(
-      null,
-      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
-    );
+    const filename = `${file.fieldname}-${Date.now()}${path.extname(
+      file.originalname
+    )}`;
+    console.log("Filename:", filename);
+    cb(null, filename);
   },
 });
 
